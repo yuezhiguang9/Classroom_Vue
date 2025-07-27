d3d7a375fabec8483f40d20b157a2a7930362808
cheng <19172009306@139.com>
2025-07-28 00:16:56 +0800

    提交创建项目时候的代码

---
+++ Campus_Management_System_Vue/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { ref } from "vue";
+
+  defineProps({
+    msg: String,
+  });
+
+  const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <p>>, the official Vue + Vite starter</p>
+    <p>
+      Learn more about IDE Support for Vue in the
+      <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank"
+        >Vue Docs Scaling up Guide</a
+      >.
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  </div>
+</template>
+
+<style scoped>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
