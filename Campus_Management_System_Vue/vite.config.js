d61c68920205c91b2a557467cb723151f967a05c
xj200412 <726419144@qq.com>
2025-08-17 17:06:41 +0800

    Login.vue更新（登录）
    ①改了一下Login.vue的组合式api写法 方便查看数据 ②引用了axios.js的方式 【axios.js位于utils文件夹下面 删掉直接在src下面的axios.js】 ③修改之后 确保登录页面不仅能返回数据 也能跳转到教秘工作台页面【其他的页面跳转等之后有vue再添加 可以先拿SecretaryListLogs.vue测试】

--- Campus_Management_System_Vue/vite.config.js
+++ Campus_Management_System_Vue/vite.config.js
@@ -4,4 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+<<<<<<< Updated upstream
 })
+=======
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081', // 后端地址
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
+>>>>>>> Stashed changes
